4541c94a0882d96e004e88355d6436f55b39ab74
Nur Muhammad <blog.nurmuhammad@gmail.com>
2022-02-21 22:50:47 +0800

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
+
 import './styles/tailwind.css'
 
-const app = createApp(App);
+const app = createApp(App)
+
+app.use(VueGtag, {
+  config: {
+    id: 'G-HPW7YZV0B2',
+    params: {
+      send_page_view: false,
+    }
+  },
+  enabled: import.meta.env.PROD
+})
 
 app.mount('#app')
